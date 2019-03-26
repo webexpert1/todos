d98899d077136dee990095248169f586d35975e5
Richie <richardokoye2@gmail.com>
2019-03-26 16:20:50 +0100

    added components and events

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+
+class TodoItems extends Component {
+	constructor(props) {
+		super(props);
+
+		this.createTasks = this.createTasks.bind(this);
+		this.delete = this.delete.bind(this);
+	}
+	createTasks(item) {
+		return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+	}
+
+	delete(key){
+		this.props.delete(key);
+	}
+
+	render() {
+		let todoEntries = this.props.entries;
+		let listItems = todoEntries.map(this.createTasks);
+
+		return(
+			<ul className="theList">{listItems}</ul>
+		)
+	}
+}
+
+export default TodoItems;
